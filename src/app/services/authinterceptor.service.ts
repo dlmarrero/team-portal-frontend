3f6c2180ccbf5bfc2e47031762a11b03f7f55bac
Davy Marrero <dleefilms@gmail.com>
2017-12-05 10:20:48 -0500

    Removed console logs from auth interceptor

--- src/app/services/authinterceptor.service.ts
+++ src/app/services/authinterceptor.service.ts
@@ -5,14 +5,11 @@ import { AccountService } from 'app/accounts/account.service';
  
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor() {
-    console.log("Inside auth service!!!!!")
-  }
+  constructor() { }
  
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // Get the auth header from the service.
     const authHeader = "Bearer " + localStorage.getItem('jwt');
-    console.log(authHeader);
     // Clone the request to add the new header.
     const authReq = req.clone({setHeaders: {Authorization: authHeader}});
     // Pass on the cloned request instead of the original request.
